bb543abef53e2a306c46d3d4f6a9ad504cb3497c
Naufal Mir <naufal.mir@wipro.com>
2018-11-25 17:29:38 +0000

    Updated: template.yaml to add DynamoDB and IAM permissions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //export AWS_XRAY_CONTEXT_MISSING=LOG_ERROR
-// const AWSXRay = require('aws-xray-sdk-core'),
-const  AWS = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk-core'),
+    AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 const documentClient = new AWS.DynamoDB.DocumentClient({
     region: process.env.AWS_REGION,
@@ -16,65 +16,64 @@ const dynamoQuery = (params) => {
     return documentClient.query(params).promise().catch(err => console.log(err))
 }
 
-const generateResponse = (status, body) => {
-  return {
-      'statusCode': status,
-      'body': JSON.stringify(body)
-  }
+const generateResponse = (statusCode, body) => {
+    return {
+        'statusCode': statusCode,
+        'body': JSON.stringify(body)
+    }
 }
 
-let response;
-
-const putTask = async (event, context, callback) => {
+const putBirthday = async (event, context, callback) => {
     try {
-      const body = JSON.parse(event.body)
-      const dOB = Date(body.dateOfBirth).format("YYYY-mm-dd")
-      const put = await dynamoPut({
-        TableName: 'birthdays',
-        Item: {
-          Id: String(event.pathParameters.userId),
-          dateOfBirth: body.dateOfBirth
-        }
-      })
+        const body = JSON.parse(event.body)
+        const put = await dynamoPut({
+            TableName: process.env.TABLE_NAME,
+            ReturnValues: "ALL_OLD",
+            Item: {
+                Id: String(event.pathParameters.userId),
+                dateOfBirth: Date(body.dateOfBirth).format('YYYY-mm-dd')
+            }
+        })
 
-      //return the returned value
-      callback(null, generateResponse(204, {
-        put
-      }))
-
+        callback(null, generateResponse(200, {
+            put
+        }))
     } catch (err) {
         console.log(err);
-        callback(null, generateResponse(500, {
-          err
+        callback(err, generateResponse(500, {
+            err
         }))
     }
 };
-const getTask = async (event, context, callback) => {
+
+const getBirthday = async (event, context, callback) => {
     try {
-      const get = await dynamoQuery({
-        TableName: 'birthdays',
-        KeyConditionExpression: 'Id = :id',
-        ExpressionAttributeValues: {
-          ':id': String(event.pathParameters.userId)
+        const get = await dynamoQuery({
+            TableName: process.env.TABLE_NAME,
+            KeyConditionExpression: 'Id = :id',
+            ExpressionAttributeValues: {
+                ':id': String(event.pathParameters.userId)
+            }
+        })
+
+
+        if(get.Count === 0) {
+            callback(null, generateResponse(404, "No record found for that name"))
         }
-      })
-      // const dOB = Date(get.dateOfBirth).format("YYYY-mm-dd")
-      //return the returned value
-      callback(null, generateResponse(204, {
-        get
-      }))
+
+        callback(null, generateResponse(200, {
+            get
+        }))
     } catch (err) {
         console.log(err);
-        callback(null, generateResponse(500, {
-          err
+        callback(err, generateResponse(500, {
+            err
         }))
     }
 };
 
 module.exports = {
-  getTask,
-  putTask,
-  generateResponse
+    getBirthday,
+    putBirthday,
+    generateResponse
 }
-
-// handler()
